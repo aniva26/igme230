0ff938eb9e492066d9df7116cb4c214bd632b7cd
Anika Niva <anikaniva@Anikas-MacBook-Pro.local>
2018-12-06 17:31:39 -0500

    added final to test article

---
+++ fall2018final/fall2018final.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('.mainmenu').on('click', function () {
+        $(this).next('.submenu').slideToggle();
+    });
+
+    let text = ("article0.txt") // sets default verse element
+//    $("#choose-content").val(text); // changes menu option to default
+    $("article").load(text); // retrieves only default element
+
+    $("form").select(function () {
+        text = $(this).val();
+        $("article").load(text);
+    });
+});
